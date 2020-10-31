4a15e9ed4a20e7b80a889e5b60b19ed1bf3d3a99
ua4aeu <ra4a@yandex.ru>
2020-10-31 13:10:46 +0300

    Add files via upload

---
+++ AutoCompleteCustomSource1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoCompleteCustomSource1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AutoCompleteStringCollection source = new AutoCompleteStringCollection()
+        {
+            "Кузнецов",
+            "Иванов",
+            "Петров",
+            "Кустов"
+        };
+            textBox1.AutoCompleteCustomSource = source;
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
